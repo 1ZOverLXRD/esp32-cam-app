1b74f900e016e9bb7f96289af12577d9c32e9e92
OverLxrd <2537785558@qq.com>
2026-06-25 16:25:27 +0800

    feat: add wifi_config.h shared constants

---
+++ components/wifi_config/wifi_config.h
@@ -0,0 +1,9 @@
+#ifndef WIFI_CONFIG_H
+#define WIFI_CONFIG_H
+
+#define WIFI_AP_SSID    "ESP32-CAM"
+#define WIFI_AP_PASS    "12345678"
+#define WIFI_AP_IP      "192.168.4.1"
+#define WIFI_AP_PORT    80
+
+#endif
